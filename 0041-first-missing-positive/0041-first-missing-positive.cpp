fe6a3fc2d537cd00fe5b77a7f4228d5351ae63d9
Prashant <60565847+prashantzzz@users.noreply.github.com>
2023-08-13 22:06:03 +0530

    Time: 60 ms (78.31%), Space: 41.3 MB (34.78%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        long long ans = static_cast<long long>(nums[nums.size() - 1]) + 1;
         int reqd=1;
         //replacing +ve duplicates with -1
         for(int i=0; i<nums.size()-1; i++){ 
