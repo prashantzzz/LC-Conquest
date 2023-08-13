fad2d82e49e2b73508bd746cdff8bd1cf0b6afa5
Prashant <60565847+prashantzzz@users.noreply.github.com>
2023-08-13 22:04:25 +0530

    Time: 60 ms (78.31%), Space: 41.1 MB (59.76%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -4,8 +4,9 @@ public:
         sort(nums.begin(),nums.end());
         long long ans = static_cast<long long>(nums[nums.size() - 1]) + 1;
         int reqd=1;
-        for(int i=0; i<nums.size()-1; i++){
-            if(nums[i]==nums[i+1])nums[i]=-1;
+        //replacing +ve duplicates with -1
+        for(int i=0; i<nums.size()-1; i++){ 
+            if(nums[i]>0 && nums[i]==nums[i+1])nums[i]=-1;
         }
         for(int i=0; i<nums.size(); i++){
             if(nums[i]>0){
@@ -13,7 +14,6 @@ public:
                 reqd++; //for duplications
             }
         }
-        if(ans>0)return ans;
-        else return 1;
+        return reqd;
     }
 };
