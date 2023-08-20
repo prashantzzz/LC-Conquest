bd74d36f132b2ef3debb877b61975fea41c93958
Prashant <60565847+prashantzzz@users.noreply.github.com>
2023-08-20 18:22:48 +0530

    Time: 29 ms (93.31%), Space: 28.2 MB (25.37%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,32 +1,19 @@
-class Solution {
+class Solution{
 public:
-    int minSubArrayLen(int target, vector<int>& nums) {
-        int min_len=INT_MAX, j=0, i=0, sum=0;
-        for(i=0; i<nums.size(); i++){
-            if(sum<target) {
-                sum+=nums[i];
-                cout<<"+"<<nums[i];
-            }
-            else if(sum>=target) {
-                min_len=min(min_len,i-j);
-                i--;
-                sum-=nums[j];
-                cout<<endl<<"-"<<nums[j];
-                j++;
+    int minSubArrayLen(int target, vector<int>& nums){
+        int min_len=INT_MAX, l=0, r=0, sum=0;
+        for(r=0; r<nums.size(); r++){
+            sum+=nums[r];
+            while(sum>=target){
+                min_len=min(min_len,r-l+1);
+                sum-=nums[l];
+                l++;
+
             }
         }
-        int n=nums.size();
-        for(int i=j;i<nums.size();i++){
-            if(sum>=target) {
-                min_len=min(min_len,n-j);
-                j++;
-                sum-=nums[i];
-            }
-            if(sum<target)break;
-        }
-        
-        cout<<endl<<sum<<" j="<<j<<endl;
-        if(min_len==INT_MAX) return 0;
+
+        if(min_len==INT_MAX)return 0;
         return min_len;
+
     }
 };
