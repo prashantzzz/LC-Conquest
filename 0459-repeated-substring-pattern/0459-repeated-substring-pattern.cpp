8d0a09dc2a9f40e3846342ae828d2fcb15da9baa
Prashant <60565847+prashantzzz@users.noreply.github.com>
2023-09-04 11:15:50 +0530

    Time: 202 ms (19.64%), Space: 522.2 MB (5.01%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool is_true(int j,string s){
+        string temp=s.substr(0,j),add=temp;
+        while(temp.length()<s.length()){
+            temp+=add;
+        }
+        if(temp==s)return true;
+        return false;
+    }
+    bool repeatedSubstringPattern(string s) {
+        int i=0,j=1;
+        while(j<s.length()){
+            if(s[i]!=s[j]) {i=0; j++;}
+            else {
+                if(is_true(j,s))return true;
+                else j++;
+            }
+                
+        }
+        return false;
+    }
+};
