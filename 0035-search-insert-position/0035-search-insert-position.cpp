5ffb54c63c117416a048d0ec92c983dc9ea7a897
Prashant <60565847+prashantzzz@users.noreply.github.com>
2023-09-23 12:36:46 +0530

    Time: 0 ms (100%), Space: 10.2 MB (9.26%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
-    int bin(vector<int>& nums,int target, int i){
-        if(nums[i]==target) return i;
-        if(nums[i]<target) i++;
-        else if(nums[i]>target) i--;
-        return 0;
-    }
     int searchInsert(vector<int>& nums, int target) {
-        int i=0,j=nums.size()-1;
+        int i=0;
         while(i<nums.size()){
             if(nums[i]>=target)return i;
             i++;
